10d497e033ab480496d8e6a6875e20f177931d1a
Vasiliy <kaktusv6@gmail.com>
2019-06-09 13:49:17 +1000

    Изменил шапку сайта

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        <b>
+          Поиск очагов
+          вирусных заболеваний</b>
       </a>
     </div>
 
@@ -25,5 +27,8 @@
 </script>
 
 <style scoped>
-
+  .navbar-item {
+    font-size: 32px;
+    color: #3498db;
+  }
 </style>
